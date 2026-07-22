efd78f77b12e5aabbbf504674b8d64c4bad00397
pakelegence <pakelegence@gmail.com>
2026-07-22 14:49:02 +0500

    resolve the loader error

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Loader = () => {
+
+  const location=useLocation()
+
+  useEffect(()=>{
+    window.scrollTo(0 , 0)
+  },[location.pathname])
+
   return (
-    <div className='w-full h-dvh flex justify-center items-center bg-white z-50'>
+    <div className='w-full fixed top-0 left-0 h-dvh flex justify-center items-center bg-white z-50'>
         <div className="loader">
 
         </div>
